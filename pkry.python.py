9d7c029a0a78363a37c9e268266811eaf1a5e5d4
piceuu <112367760+piceuu@users.noreply.github.com>
2022-09-01 20:23:29 -0700

    fikry
    tugas pemdas python5
    kls:x tkj3

---
+++ pkry.python.py
@@ -0,0 +1,39 @@
+# Rumus Persegi Sisi * Sisi
+
+sisi1 = 10
+sisi2 = 10
+luas_p = sisi1 * sisi2
+
+print("luas persegi adalah", luas_p)
+
+# Rumus Segitiga A * T / 2
+
+alas = 20
+tinggi = 10
+luas_s = alas * tinggi / 2
+
+print("luas segitiga adalah", luas_s)
+
+# Rumus Jajargenjang Alas * Tinggi
+
+alas = 30
+tinggi = 15
+luas_j = alas * tinggi
+
+print("luas jajargenjang adalah", luas_j)
+
+# Rumus Lingkaran phi * r *
+
+phi = 22/7
+r = 14
+luas_l = phi * r * r
+
+print("luas lingkaran adalah", luas_l)
+
+
+# Program Dinamis
+
+print("\n1.Luas Persegi")
+sisi = int(input("Masukan nilai sisi: "))
+luas_p = sisi * sisi
+print("Luas Persegi adalah", luas_p)
