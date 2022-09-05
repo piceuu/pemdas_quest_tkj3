d8168876f6e2c3add08c5d527e10a366a8290fc5
piceuu <112367760+piceuu@users.noreply.github.com>
2022-09-04 22:15:43 -0700

    Tugas Pemdas

---
+++ python6-tkj3.py
@@ -0,0 +1,25 @@
+# Dolar Ke Rupiah
+
+
+dolar = int(input("masukan nilai dolar: "))
+rupiah = dolar * 15000
+
+print  ("Dolar Ke Rupiah" , rupiah)
+
+
+# Euro Ke Won
+
+
+euro = int(input("masukan nilai euro: "))
+won = euro * 2714
+
+print  ("Euro Ke Won" , won)
+
+
+# Yen Ke Rupiah
+
+
+yen = int(input("masukan nilai yen: "))
+rupiah = yen * 7368
+
+print  ("Yen Ke Rupiah" , rupiah)
