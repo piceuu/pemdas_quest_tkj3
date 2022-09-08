19f9f95bb3c2d153f226f451d0e36df333ae3ded
piceuu <112367760+piceuu@users.noreply.github.com>
2022-09-07 23:41:49 -0700

    Add files via upload

---
+++ latihan.py
@@ -0,0 +1,40 @@
+#Persegi
+
+print("\n1. LUAS PERSEGI")
+sisi = int(input("masukan nilai sisi: "))
+luas_p = sisi * sisi
+print("luas persegi adalah", luas_p)
+
+#Segitiga
+
+print("\n2. LUAS SEGITIGA")
+alas = int(input("masukan nilai alas: "))
+tinggi = int(input("masukan nilai tinggi: "))
+luas_s = alas * tinggi
+print("luas segitiga adalah", luas_s)
+
+#Jajar Genjang
+
+print("\n3. LUAS JAJARGENJANG")
+alas = int(input("masukan nilai alas: "))
+tinggi = int(input("masukan nilai tinggi: "))
+luas_j = alas * tinggi
+print("luas jajar genjang adalah", luas_j)
+
+
+#Lingkaran
+
+print("\n4. LUAS LINGKARAN")
+phi = 22/7
+r = int(input("masukan nilai r: "))
+luas_l = phi * r * r
+print("luaas lingkaran adalah", luas_l)
+
+# Persegi panjang
+
+print("\n5. LUAS PERSEGIPANJANG")
+panjang = int(input("masukan nilai panjang: "))
+lebar = int(input("masukan nilai lebar: "))
+luas_pp = panjang * lebar
+print("luas persegi panjang adalah", luas_pp)
+
